979d7eb186aa4e86b5486ccda5b21fa83655861c
HPBenjamin <hptischoff@gmail.com>
2021-07-06 17:48:23 +0200

    added user-array

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+import { User } from 'src/models/user.class';
+import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 
 @Component({
   selector: 'app-user',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
+  user: User = new User();
+  birthDate: Date;
+  constructor(public dialog: MatDialog) { }
 
-  constructor() { }
+
 
   ngOnInit(): void {
+  }
+
+  openDialog() {
+    this.dialog.open(DialogAddUserComponent);
   }
 
 }
